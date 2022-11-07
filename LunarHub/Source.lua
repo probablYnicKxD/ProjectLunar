ac3ba80326117e81ed05b77d9996db8fbc544fb9
probablYnicK <112222531+probablYnicKxD@users.noreply.github.com>
2022-11-07 18:09:13 -0500

    Update Source.lua

--- LunarHub/Source.lua
+++ LunarHub/Source.lua
@@ -4,6 +4,53 @@
 
 ]]--
 
+--[[ Configurations ]]--
+
+local LunarHubSavingFolderName = "LunarHub Configurations"
+
+--[[ Loading LunarHub + Protection + Configuration Saving ]]--
+
+local LunarHub = game:GetObjects("rbxassetid://0")[1]
+
+--Protection--
+
+if gethui then
+	LunarHub.Parent = gethui()
+elseif syn.protect_gui then
+	syn.protect_gui(LunarHub)
+	LunarHub.Parent = game.CoreGui
+else
+	LunarHub.Parent = game.CoreGui
+end
+
+--Configuration Saving--
+
+if makefolder and isfolder then
+  
+else
+    warn("LunarHub // Could not make configurations folder for LunarHub!")
+end
+
+--[[ Playerlist System ]]--
+
+local playerList = game.Players:GetPlayers()
+local usingLunarList = {}
+
+for i, player in pairs(playerList) do
+    player.Chatted:Connect(function(msg)
+        msg = string.lower(msg)
+      
+        if msg == "___" then
+            table.insert(usingLunarList, player)
+            print("Detected " .. player.DisplayName .. " (@" .. player.Name .. ") is using Lunar!"
+        end
+    end)
+end
+  
+
+
+--[[ Using Lunar Detection System ]]--
+
 local function chat(msg)
   game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(msg, "All")
 end
