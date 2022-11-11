e46e8a6268636fa465224b29b1f4dfcc4f843184
probablYnicK <112222531+probablYnicKxD@users.noreply.github.com>
2022-11-11 18:45:59 -0500

    added 13 games + added game creator tag

--- LunarHub/QuickPlayGames.lua
+++ LunarHub/QuickPlayGames.lua
@@ -1,3 +1,13 @@
+--[[
+
+    game libary handwritten by probablYnicK
+
+    dm me if you want me to add a game to the library
+
+    discord: lunaR_nicK#6800
+
+]]--
+
 local quickPlayGames = {}
 
 quickPlayGames.Games = {}
@@ -12,13 +22,14 @@ function quickPlayGames:JoinGame(gameID, jobID)
     end
 end
 
-function quickPlayGames:AddGame(name, gameID)
+function quickPlayGames:AddGame(name, gameID, gameCreator)
     if gameID == 0 then return end
 
     local success,errMsg = pcall(function()
         quickPlayGames.Games[name] = {
             Name = name,
-            GameID = gameID
+            GameID = gameID,
+            GameCreator = gameCreator,
         }
         
         local gm = quickPlayGames[name]
@@ -46,26 +57,39 @@ function quickPlayGames:GetGamePlayers(gameID)
     return plrs[0].playing
 end
 
-local function addGame(name, gameID)
-    return quickPlayGames:AddGame(name, gameID)
+local function addGame(name, gameID, gameCreator)
+    return quickPlayGames:AddGame(name, gameID, gameCreator)
 end
 
-addGame("DOORS 👁", 6516141723)
-addGame("Evade", 9872472334)
-addGame("Adopt Me!", 0) --getting next script upd because am lazy 💀
-addGame("Blox Fruits", 2753915549)
-addGame("Da Hood", 2788229376)
-addGame("Prison Life", 155615604)
-addGame("Ninja Legends", 3956818381)
-addGame("Ninja Legends 2", 5977280685)
-addGame("BedWars", 6872265039)
-addGame("Ro-Ghoul", 914010731)
-addGame("Funky Friday", 6447798030)
-addGame("Mad City", 1224212277)
-addGame("Bee Swarm Simulator", 1537690962)
-addGame("Auto Rap Battles", 579955134)
-addGame("Doomspire", 1215581239)
-addGame("Jailbreak", 606849621)
-addGame("Lumber Tycoon", 13822889)
+addGame("DOORS 👁", 6516141723, "LSPLASH")
+addGame("Evade", 9872472334, "Hexagon Development Community")
+addGame("Adopt Me!", 920587237, "DreamCraft")
+addGame("Blox Fruits", 2753915549, "Gamer Robot Inc")
+addGame("Da Hood", 2788229376, "Da Hood Entertainment")
+addGame("Prison Life", 155615604, "Aesthetical")
+addGame("Ninja Legends", 3956818381, "Scriptbloxian Studios")
+addGame("Ninja Legends 2", 5977280685, "Scriptbloxian Studios")
+addGame("BedWars", 6872265039, "Easy.gg")
+addGame("Ro-Ghoul", 914010731, "SushiWalrus")
+addGame("Funky Friday", 6447798030, "Lyte Interactive")
+addGame("Mad City", 1224212277, "Schwifty Studios")
+addGame("Bee Swarm Simulator", 1537690962, "Onett")
+addGame("Auto Rap Battles", 579955134, "Auto Rap Battles Community")
+addGame("Doomspire Brickbattle", 1215581239, "Temple of Brickbattle")
+addGame("Jailbreak", 606849621, "Badimo")
+addGame("Lumber Tycoon 2", 13822889, "Defaultio")
+addGame("Pet Simulator X", 6284583030, "BIG Games Pets")
+addGame("Brookhaven", 4924922222, "Wolfpaq")
+addGame("Rainbow Friends", 7991339063, "Fragment Games")
+addGame("Livetopia", 6737970321, "Century Makers")
+addGame("Murder Mystery 2", 142823291, "Nikilis")
+addGame("MeepCity", 370731277, "alexnewtron")
+addGame("Build A Boat For Treasure", 537413528, "Chillz Studios")
+addGame("Flee the Facility", 893973440, "A.W. Apps")
+addGame("Arsenal", 286090429, "ROLVe")
+addGame("Tower of Hell", 1962086868, "YXCeptional Studios")
+addGame("PLS DONATE", 8737602449, "haz3mn")
+addGame("3008", 2768379856, "uglyburger0")
+addGame("Plane Crazy", 166986752, "madattak")
 
 return quickPlayGames
