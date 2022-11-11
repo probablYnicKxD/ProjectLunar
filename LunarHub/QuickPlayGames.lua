008534026323ffdf5abaffb3eb3ade52e7748d20
probablYnicK <112222531+probablYnicKxD@users.noreply.github.com>
2022-11-10 19:46:03 -0500

    Create QuickPlayGames.lua
    This is a "game library" for LunarHub's QuickPlay feature. The QuickPlay feature will allow you to play a game with the click of a button.

---
+++ LunarHub/QuickPlayGames.lua
@@ -0,0 +1,69 @@
+local quickPlayGames = {}
+
+function quickPlayGames:JoinGame(gameID, jobID)
+    if jobID then
+        print("LunarHub // Joining server with server ID: " .. jobID .. "...")
+        game:GetService('TeleportService'):TeleportToPlaceInstance(gameID, jobID, game.Players.LocalPlayer)
+    else
+        print("LunarHub // Joining game with ID: " .. gameID .. "...")
+        game:GetService('TeleportService'):Teleport(gameID, game.Players.LocalPlayer)
+    end
+end
+
+function quickPlayGames:AddGame(name, gameID)
+    if gameID == 0 then return end
+
+    local success,errMsg = pcall(function()
+        quickPlayGames[name] = {
+            Name = name,
+            GameID = gameID
+        }
+        
+        local gm = quickPlayGames[name]
+        
+        if gm then
+            function gm:JoinGame()
+                quickPlayGames:JoinGame(gameID)
+            end
+        end
+    end)
+    
+    if success then
+        return quickPlayGames[name]
+    elseif not success and errMsg then
+        warn("Failed to add game // Error Message:\n" .. errMsg)
+        return nil
+    end
+end
+
+function quickPlayGames:GetGamePlayers(gameID)
+    local httpService = game:GetService("HttpService")
+    
+    local plrs = httpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games?universeIds=" .. gameID))
+    
+    return plrs[0].playing
+end
+
+local function addGame(name gameID)
+    return quickPlayGames:AddGame(name, gameID)
+end
+
+addGame("DOORS 👁", 6516141723)
+addGame("Evade", 9872472334)
+addGame("Adopt Me!", 0) --unknown game id as of now, getting it later because am lazy
+addGame("Blox Fruits", 2753915549)
+addGame("Da Hood", 2788229376)
+addGame("Prison Life", 155615604)
+addGame("Ninja Legends", 3956818381)
+addGame("Ninja Legends 2", 5977280685)
+addGame("BedWars", 6872265039)
+addGame("Ro-Ghoul", 914010731)
+addGame("Funky Friday", 6447798030)
+addGame("Mad City", 1224212277)
+addGame("Bee Swarm Simulator", 1537690962)
+addGame("Auto Rap Battles", 579955134)
+addGame("Doomspire", 1215581239)
+addGame("Jailbreak", 606849621)
+addGame("Lumber Tycoon", 13822889)
+
+return quickPlayGames
