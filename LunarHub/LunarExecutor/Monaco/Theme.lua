c65978c79e2319aefb284dff537c420dae29686e
probablYnicK <112222531+probablYnicKxD@users.noreply.github.com>
2022-11-27 12:54:52 -0500

    Update Theme.lua

--- LunarHub/LunarExecutor/Monaco/Theme.lua
+++ LunarHub/LunarExecutor/Monaco/Theme.lua
@@ -6,7 +6,8 @@ local theme = {
 			["string"] = Color3.fromRGB(130, 241, 149),
 			["number"] = Color3.fromRGB(255, 198, 0),
 			["comment"] = Color3.fromRGB(106, 106, 100),
-			["thingy"] = Color3.fromRGB(253, 251, 154)
+			["thingy"] = Color3.fromRGB(253, 251, 154),
+			["custom"] = Color3.fromRGB(170, 221, 255),
 		},
 		["extra 2"] = {
 			["keyword"] = Color3.fromRGB(249, 36, 114),
@@ -14,7 +15,8 @@ local theme = {
 			["string"] = Color3.fromRGB(217, 219, 88),
 			["number"] = Color3.fromRGB(161, 118, 209),
 			["comment"] = Color3.fromRGB(116, 122, 101),
-			["thingy"] = Color3.fromRGB(248, 245, 139)
+			["thingy"] = Color3.fromRGB(248, 245, 139),
+			["custom"] = Color3.fromRGB(170, 221, 255),
 		}
 	}
 }
