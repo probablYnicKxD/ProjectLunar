a03158d3f83b1d2de34e9bc62c909574f69101dc
probablYnicK <112222531+probablYnicKxD@users.noreply.github.com>
2022-11-27 10:48:18 -0500

    Create Words.lua

---
+++ LunarHub/LunarExecutor/Monaco/Words.lua
@@ -0,0 +1,36 @@
+--[[
+  some words added by probablYnicK
+]]--
+
+local words = {
+	['print'] = 'builtin',
+	['warn'] = 'builtin',
+	['Vector3'] = 'builtin',
+	['Vector2'] = 'builtin',
+  ['UDim'] = 'builtin',
+  ['UDim2'] = 'builtin',
+	['error'] = 'builtin',
+	['Instance'] = 'builtin',
+	['game'] = 'builtin',
+	['script'] = 'builtin',
+	['workspace'] = 'builtin',
+  ['assert'] = 'builtin',
+  ['task'] = 'builtin',
+  ['wait'] = 'builtin',
+  ['pairs'] = 'builtin',
+  ['ipairs'] = 'builtin',
+	
+	['while'] = 'keyword',
+	['true'] = 'keyword',
+	['false'] = 'keyword',
+	['then'] = 'keyword',
+	['do'] = 'keyword',
+	['if'] = 'keyword',
+  ['end'] = 'keyword',
+  ['elseif'] = 'keyword',
+  ['else'] = 'keyword',
+  ['return'] = 'keyword',
+  ['local'] = 'keyword',
+}
+
+return words
